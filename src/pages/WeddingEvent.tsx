872616159dad21e7b31de09a17bf9efe793cdeef
johengrace <johengrace@gmail.com>
2021-07-23 00:31:30 +0200

    Refactor position of wedding event section using bootstrap

--- src/pages/WeddingEvent.tsx
+++ src/pages/WeddingEvent.tsx
@@ -1,33 +1,32 @@
-import { Row, Col } from 'antd';
 import YoutubeEmbed from '../component/YoutubeEmbed';
 import { Button, Radio } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const WEvent = () => {
     return(
-        <div className="event">
-           <Row className="weddingEvent">
-                <Col xs={24} xl={12} className="invitation">
-                    <div className="invitationContent">
-                        <h4 className="invitationText">By the grace of God, <br/> We cordially invite you to join us in our Holy Matrimony</h4>
-                        <h1 className="invitationName">Joshua Hendinata</h1>
-                        <h4>Son of Mr. Hadi Sucianto and Mrs. Erviany</h4>
-                        <h1 style={{margin:"-5px"}}>&</h1>
-                        <h1 className="invitationName">Grace Christina</h1>
-                        <h4>Daughter of Mr. Sutopo Hermanto and Mrs. Minawati Yap</h4>
-                        <h3 className="invitationText">Saturday, 2 October 2021 <br/>  Holy Matrimony at 10am | Bukit Batok Presbyterian Church </h3>
-                    </div>
-                </Col>
-                <Col xs={24} xl={12} className="youtubeVideo">
-                    <YoutubeEmbed/>
-                    <Button type="primary" icon={<DownloadOutlined />}>
-                        Download Booklet
-                    </Button>
-                    <Button>Add To Calendar</Button>
-                </Col>
-            </Row>
-        </div>
-
+        <Container fluid>
+            <div className="event">
+                <Row className="weddingEvent my-5">
+                        <Col lg={true} className="invitation text-center">
+                            <p className="lh-sm fs-4 mt-5">By the grace of God, <br/> We cordially invite you to join us in our Holy Matrimony</p>
+                            <p className="invitationName my-0 ">Joshua Hendinata</p>
+                            <p className="parents lh-sm fs-4">Son of Mr. Hadi Sucianto and Mrs. Erviany</p>
+                            <p className="invitationAnd my-0">&</p>
+                            <p className="invitationName my-0">Grace Christina</p>
+                            <p className="parents lh-sm fs-4">Daughter of Mr. Sutopo Hermanto and Mrs. Minawati Yap</p>
+                            <p className="mt-0 fs-3 fw-bold">Saturday, 2 October 2021 <br/>  Holy Matrimony at 10am | Bukit Batok Presbyterian Church </p>
+                        </Col>
+                        <Col lg={true} className="youtubeVideo my-5">
+                            <YoutubeEmbed/>
+                            <Button type="primary" icon={<DownloadOutlined />}>
+                                Download Booklet
+                            </Button>
+                            <Button>Add To Calendar</Button>
+                        </Col>
+                </Row>
+            </div>
+        </Container>
 
 
         
