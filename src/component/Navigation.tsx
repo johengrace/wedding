e8cadcc112e5acef209dea8a9c5b8970ad717a62
johengrace <johengrace@gmail.com>
2021-07-23 01:37:31 +0200

    Add navbar brand, remove antd and unused classes

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,54 +1,54 @@
-import 'antd/dist/antd.css';
 import { Button } from 'react-bootstrap';
 import { Container, Nav, Navbar } from "react-bootstrap";
-// import { Link as ScrollLink} from "react-scroll";
 import { scroller } from "react-scroll";
 
 const NavBar = () => {
     return(
         <Container>
             <Navbar expand="lg" bg="light" variant="light" fixed="top" >
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                <Navbar.Brand className="d-block d-sm-block d-md-block d-lg-none d-xl-none py-0 mx-3 brand">Joshua & Grace</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-2" />
+                <Navbar.Collapse id="basic-navbar-nav " className="justify-content-center">
                     <Nav defaultActiveKey="/home">
-                        <Nav.Link 
+                        <Nav.Link
+                        className="mx-2"
                         eventKey="home"
                         onSelect={() => scroller.scrollTo('home', {
-                            smooth: true,
                             offset: 0,
-                            duration: 500,
+                            duration: 200,
+                            delay: 0,
                         })}
                         >
                             Home
                         </Nav.Link>
                         <Nav.Link 
+                        className="mx-2"
                         eventKey="event"
                         onSelect={() => scroller.scrollTo('event', {
-                        smooth: true,
                         offset: -30,
-                        duration: 500,
+                        duration: 200,
+                        delay: 0,
                         })}
                         >
                             Event
                         </Nav.Link>
                         <Nav.Link 
+                        className="mx-2"
                         eventKey="story"
                         onSelect={() => scroller.scrollTo('story', {
-                        smooth: true,
-                        offset: -70,
-                        duration: 500,
+                        offset: -30,
+                        duration: 200,
                         })}
                         >
                             Our Story
                         </Nav.Link>
                         <Button 
-                        className="navbar-right"
+                        className="navbar-right mx-2"
                         variant="outline-primary"
                         size="sm"
                         onClick={() => scroller.scrollTo('rsvp', {
-                        smooth: true,
-                        offset: -70,
-                        duration: 500,
+                        offset: -30,
+                        duration: 200,
                         })}>
                             RSVP
                         </Button>
