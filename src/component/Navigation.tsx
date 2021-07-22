def47221e47b50ebab8940e9c5d3a2e669e1e25f
johengrace <johengrace@gmail.com>
2021-07-22 23:35:37 +0200

    Refactor Navbar to use bootstrap

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,81 +1,62 @@
-import { Button, Menu } from "antd";
 import 'antd/dist/antd.css';
+import { Button } from 'react-bootstrap';
 import { Container, Nav, Navbar } from "react-bootstrap";
 // import { Link as ScrollLink} from "react-scroll";
 import { scroller } from "react-scroll";
 
 const NavBar = () => {
     return(
-        // <div className="menu">
-        // <Menu mode="horizontal">
-        //     <Menu.Item key="home">
-        //         <ScrollLink to="home" smooth={true}>
-        //             Home
-        //         </ScrollLink>
-        //     </Menu.Item>
-        //     <Menu.Item key="event">
-        //         <ScrollLink to="event" smooth={true} offset={40}>
-        //             Event
-        //         </ScrollLink>
-        //     </Menu.Item>
-        //     <Menu.Item key="story">
-        //         <ScrollLink to="story"  smooth={true}>
-        //             Our Story
-        //         </ScrollLink>
-        //     </Menu.Item>
-        //     <Button className="rsvp-button" type="primary">
-        //         <ScrollLink to="rsvp" smooth={true}>
-        //             RSVP
-        //         </ScrollLink>
-        //     </Button>
-            
-        // </Menu>
-        // </div>
-        <Navbar bg="light" variant="light" fixed="top" >
-            <Navbar.Collapse className="justify-content-center">
-                <Nav defaultActiveKey="/home">
-                    <Nav.Link 
-                    eventKey="home"
-                    onSelect={() => scroller.scrollTo('home', {
+        <Container>
+            <Navbar expand="lg" bg="light" variant="light" fixed="top" >
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                    <Nav defaultActiveKey="/home">
+                        <Nav.Link 
+                        eventKey="home"
+                        onSelect={() => scroller.scrollTo('home', {
+                            smooth: true,
+                            offset: -70,
+                            duration: 500,
+                        })}
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link 
+                        eventKey="event"
+                        onSelect={() => scroller.scrollTo('event', {
                         smooth: true,
                         offset: -70,
                         duration: 500,
-                    })}
-                    >
-                        Home
-                    </Nav.Link>
-                    <Nav.Link 
-                    eventKey="event"
-                    onSelect={() => scroller.scrollTo('event', {
-                    smooth: true,
-                    offset: -70,
-                    duration: 500,
-                    })}
-                    >
-                        Event
-                    </Nav.Link>
-                    <Nav.Link 
-                    eventKey="story"
-                    onSelect={() => scroller.scrollTo('story', {
-                    smooth: true,
-                    offset: -70,
-                    duration: 500,
-                    })}
-                    >
-                        Our Story
-                    </Nav.Link>
-                    <Nav.Link 
-                    eventKey="rsvp"
-                    onSelect={() => scroller.scrollTo('rsvp', {
-                    smooth: true,
-                    offset: -70,
-                    duration: 500,
-                    })}>
-                        RSVP
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                        })}
+                        >
+                            Event
+                        </Nav.Link>
+                        <Nav.Link 
+                        eventKey="story"
+                        onSelect={() => scroller.scrollTo('story', {
+                        smooth: true,
+                        offset: -70,
+                        duration: 500,
+                        })}
+                        >
+                            Our Story
+                        </Nav.Link>
+                        <Button 
+                        className="navbar-right"
+                        variant="outline-primary"
+                        size="sm"
+                        onClick={() => scroller.scrollTo('rsvp', {
+                        smooth: true,
+                        offset: -70,
+                        duration: 500,
+                        })}>
+                            RSVP
+                        </Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
+        
     )
 }
 
